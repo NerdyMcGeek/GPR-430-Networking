697923c2986732c3b1d995478e6e2ab1ac8024df
Alex Wood <alexander.wood@mymail.champlain.edu>
2021-02-02 13:13:54 -0500

    Adding work from class

--- GPRO Net SDK/source/gpro-net-Console-Client/main-client.cpp
+++ GPRO Net SDK/source/gpro-net-Console-Client/main-client.cpp
@@ -31,12 +31,50 @@
 
 
 #include "RakNet/RakPeerInterface.h"
+#include "RakNet/MessageIdentifiers.h"
 
+#define MAX_CLIENTS 10
+#define SERVER_PORT 4024
 
 int main(int const argc, char const* const argv[])
 {
+	//tutorial one stuff
+	char str[512];
+	RakNet::RakPeerInterface* peer = RakNet::RakPeerInterface::GetInstance();
+	RakNet::Packet* packet;
 
+	RakNet::SocketDescriptor sd;
+	peer->Startup(1, &sd, 1);
 
+	//tutorial 2 stuff
+	printf("Starting the client\n");
+	strcpy(str, "172.16.2.56"); //hardcoded here should be replaced
+	peer->Connect(str, SERVER_PORT, 0, 0);
+
+	while (1)
+	{
+		for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
+		{
+			switch (packet->data[0])
+			{
+			case ID_CONNECTION_REQUEST_ACCEPTED:
+				printf("Our connection request has been accepted.\n");
+				break;
+			case ID_NO_FREE_INCOMING_CONNECTIONS:
+				printf("The server is full.\n");
+				break;
+			case ID_DISCONNECTION_NOTIFICATION:
+				printf("We have been disconnected.\n");
+				break;
+			case ID_CONNECTION_LOST:
+				printf("Connection lost.\n");
+				break;
+			default:
+				printf("Message with identifier %i has arrived.\n", packet->data[0]);
+				break;
+			}
+		}
+	}
 	printf("\n\n");
 	system("pause");
 }
--- GPRO Net SDK/source/gpro-net-Console-Server/main-server.cpp
+++ GPRO Net SDK/source/gpro-net-Console-Server/main-server.cpp
@@ -31,12 +31,57 @@
 
 
 #include "RakNet/RakPeerInterface.h"
+#include "RakNet/MessageIdentifiers.h"
 
+#define MAX_CLIENTS 10
+#define SERVER_PORT 4024
 
 int main(int const argc, char const* const argv[])
 {
+	//tutorial part 1 stuff
+	RakNet::RakPeerInterface* peer = RakNet::RakPeerInterface::GetInstance();
+	RakNet::Packet* packet;
 
+	RakNet::SocketDescriptor sd(SERVER_PORT, 0);
+	peer->Startup(MAX_CLIENTS, &sd, 1);
 
+	//tutorial part 2 stuff
+	printf("Startign the server\n");
+	peer->SetMaximumIncomingConnections(MAX_CLIENTS);
+
+	while (1)
+	{
+		for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
+		{
+			switch (packet->data[0])
+			{
+			case ID_REMOTE_DISCONNECTION_NOTIFICATION:
+				printf("Another client has disconnected.\n");
+				break;
+			case ID_REMOTE_CONNECTION_LOST:
+				printf("Another client has lost the connection.\n");
+				break;
+			case ID_REMOTE_NEW_INCOMING_CONNECTION:
+				printf("Another client has connected.\n");
+				break;
+			case ID_NEW_INCOMING_CONNECTION:
+				printf("A connection is incoming.\n");
+				break;
+			case ID_NO_FREE_INCOMING_CONNECTIONS:
+				printf("The server is full.\n");
+				break;
+			case ID_DISCONNECTION_NOTIFICATION:
+				printf("A client has disconnected.\n");
+				break;
+			case ID_CONNECTION_LOST:
+				printf("A client lost the connection.\n");
+				break;
+			default:
+				printf("Message with identifier %i has arrived.\n", packet->data[0]);
+				break;
+			}
+		}
+	}
 	printf("\n\n");
 	system("pause");
 }
